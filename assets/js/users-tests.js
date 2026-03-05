72f06016e47acc0d5ccb0b6a376ae8a2e57fa8c6
PitherMachado <128985277+PitherMachado@users.noreply.github.com>
2026-03-05 11:13:45 -0300

    Create users-tests.js

---
+++ assets/js/users-tests.js
@@ -0,0 +1,6 @@
+// assets/js/users-tests.js
+window.N1TestUsers = [
+  // EXEMPLO — você troca e cria quantos quiser
+  { user: "teste01", pass: "1234", name: "Aluno Teste 01" },
+  { user: "teste02", pass: "1234", name: "Aluno Teste 02" }
+];
